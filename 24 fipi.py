6b208b72105eb04645fdf80763fe4fa88d13cb81
iliakakurin <49334425+iliakakurin@users.noreply.github.com>
2019-04-06 13:21:16 +0500

    Update 24 fipi.py

--- 24 fipi.py
+++ 24 fipi.py
@@ -1,12 +1,12 @@
 n = int(input())
-minDigit = n % 10
+minDigit = 9
 while n > 0:
     digit =  n % 10
     if digit % 2 == 0:
         if digit < minDigit:
             minDigit = digit
     n = n // 10
-if minDigit == 0:
+if minDigit == 9:
     print("NO")
 else:
     print(minDigit)
